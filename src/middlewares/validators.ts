4215cd3dff6a5f79dfb9c0b50aa0dad4e78bc9b6
Simone Brancati <simonebrancati90@gmail.com>
2024-08-07 13:01:32 +0200

    Added validator middleware for theGuardianRoute

---
+++ src/middlewares/validators.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { validateSectionKey } from "../validation/rssService.validation";
+
+export const theGuardianSectionValidator = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { section } = req.params;
+  if (!validateSectionKey(section)) {
+    return res
+      .status(404)
+      .send({ message: `Requested section "${section}" is invalid` });
+  }
+
+  next();
+};
--- src/routes/theGuardianRss.router.ts
+++ src/routes/theGuardianRss.router.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { getSection } from "../controllers/rssFeed.controller";
+import { theGuardianSectionValidator } from "../middlewares/validators";
 
 const router = express.Router();
 
-router.get("/:section", getSection);
+router.get("/:section", theGuardianSectionValidator, getSection);
 
 export default router;
