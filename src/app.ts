b4d23930969bdee11a0d64c1deddb41d8fba63d5
Simone Brancati <simonebrancati90@gmail.com>
2024-08-07 14:21:13 +0200

    Implemented logging middleware to write to stdout logs for each requests

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import express, { Express } from "express";
 import theGuardianRssRouter from "./routes/theGuardianRss.router";
 import rssErrorHandler from "./middlewares/rssErrorHandler";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import logger from "./middlewares/logger";
 
 const app: Express = express();
+
+// Custom logger
+app.use(logger);
 
 app.use("/rss/", theGuardianRssRouter);
 
