eef231ccc117ada158035419a238cb3747f4b34c
Simone Brancati <simonebrancati90@gmail.com>
2024-08-07 11:14:51 +0200

    Implemented middlewares for error handling in the app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import "dotenv/config";
 import express, { Express } from "express";
 import theGuardianRssRouter from "./routes/theGuardianRss.router";
+import rssErrorHandler from "./middlewares/rssErrorHandler";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app: Express = express();
 
-app.use("/", theGuardianRssRouter);
+app.use("/rss/", theGuardianRssRouter);
+
+// Error Handler for theGuardiansRouter
+app.use("/rss/", rssErrorHandler);
+
+// Generic error handler
+app.use(globalErrorHandler);
 
 const port = 3000;
 
