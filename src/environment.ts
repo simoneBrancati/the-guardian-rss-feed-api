cd3e811e7756143febe88a4ec1576195ac1e459f
Simone Brancati <simonebrancati90@gmail.com>
2024-08-08 20:47:55 +0200

    Added cache ttl and debug mode environment variables

--- src/environment.ts
+++ src/environment.ts
@@ -4,6 +4,10 @@ export const redisHost = process.env.REDIS_HOST!;
 
 export const redisPort = parseInt(process.env.REDIS_PORT!);
 
+export const cacheTTLInSeconds = parseInt(process.env.CACHE_TTL!);
+
 export const theGuardianApiKey = process.env.THE_GUARDIAN_API_KEY!;
 
 export const theGuardianEndpoint = process.env.THE_GUARDIAN_SECTION_ENDPOINT!;
+
+export const debugMode = !!parseInt(process.env.DEBUG_MODE!);
