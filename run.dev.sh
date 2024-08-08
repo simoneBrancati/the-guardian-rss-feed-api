c8cc789f3150f0b15933ddcbe9f1ee4fd4d59cd8
Simone Brancati <simonebrancati90@gmail.com>
2024-08-08 20:49:37 +0200

    Updated runner scripts to allow debug mode

--- run.dev.sh
+++ run.dev.sh
@@ -7,4 +7,11 @@ if [ -z "$API_KEY" ]; then
     exit 1
 fi
 
-THE_GUARDIAN_API_KEY=$API_KEY docker compose -f docker-compose.development.yml up --build
+if [ -z "$2" ] || [ "$2" = "0" ] ; then
+    DEBUG_MODE=0
+else
+   echo "DEBUG Mode Activated"
+   DEBUG_MODE=1
+fi
+
+THE_GUARDIAN_API_KEY=$API_KEY DEBUG_MODE=$DEBUG_MODE docker compose -f docker-compose.development.yml up --build
--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-THE_GUARDIAN_API_KEY=$1
-if [ -z "$THE_GUARDIAN_API_KEY" ]; then
+API_KEY=$1
+if [ -z "$API_KEY" ]; then
     echo "ERROR: The Guardian Api Key must be set"
     exit 1
 fi
 
-docker compose up
+if [ -z "$2" ] || [ "$2" = "0" ] ; then
+    DEBUG_MODE=0
+else
+   echo "DEBUG Mode Activated"
+   DEBUG_MODE=1
+fi
+
+THE_GUARDIAN_API_KEY=$API_KEY DEBUG_MODE=$DEBUG_MODE docker compose up
