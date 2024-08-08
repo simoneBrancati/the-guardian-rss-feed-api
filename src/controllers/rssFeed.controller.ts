e3e29858d7dc388847eb8863de77b9b1b3926779
Simone Brancati <simonebrancati90@gmail.com>
2024-08-08 20:50:16 +0200

    Added rss caching in the controller

--- src/controllers/rssFeed.controller.ts
+++ src/controllers/rssFeed.controller.ts
@@ -1,6 +1,12 @@
+import {
+  getString as getCachedString,
+  setString as setStringInCache,
+} from "../ports/cache";
 import { generateRss } from "../services/rssGeneration.service";
 import { getSectionFromTheGuardian } from "../services/theGuardianApi.service";
 import { NextFunction, Request, Response } from "express";
+import logger from "../utils/logger";
+import { cacheTTLInSeconds } from "../environment";
 
 export const getSection = async (
   req: Request,
@@ -9,7 +15,7 @@ export const getSection = async (
 ): Promise<Response | null> => {
   const { section } = req.params;
   try {
-    const rss = await createRssFeed(section);
+    const rss = await getRssFeed(section);
     res.setHeader("Content-Type", "application/xml").send(rss);
 
     return res;
@@ -18,6 +24,32 @@ export const getSection = async (
 
     return null;
   }
+};
+
+const getRssFeed = async (sectionKey: string): Promise<string> => {
+  // Get rss feed from cache
+  let rss = await getCachedString(sectionKey);
+  if (rss) {
+    logger.debug(`Rss for section '${sectionKey}' found in cache`);
+    return rss;
+  }
+
+  // If rss feed is not cached, create it
+  rss = await createRssFeed(sectionKey);
+
+  // Cache rss feed and return it
+  const cacheSuccess = await setStringInCache(
+    sectionKey,
+    rss,
+    cacheTTLInSeconds,
+  );
+  if (!cacheSuccess) {
+    logger.warn("Failed to cache rss feed");
+  } else {
+    logger.debug(`Rss for section '${sectionKey}' cached successfully`);
+  }
+
+  return rss;
 };
 
 const createRssFeed = async (sectionKey: string): Promise<string> => {
