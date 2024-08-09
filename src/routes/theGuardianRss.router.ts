bc834b575a5da058507b98d3b4725c55d9672d5f
Simone Brancati <simonebrancati90@gmail.com>
2024-08-09 14:24:40 +0200

    Added cors to handle traffic from github page

--- src/routes/theGuardianRss.router.ts
+++ src/routes/theGuardianRss.router.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { getSection } from "../controllers/rssFeed.controller";
 import { theGuardianSectionValidator } from "../middlewares/validators";
+import cors, { CorsOptions } from "cors";
 
 const router = express.Router();
 
-router.get("/:section", theGuardianSectionValidator, getSection);
+const corsOptions: CorsOptions = {
+  origin: "https://simonebrancati.github.io/",
+  methods: ["GET", "OPTIONS"],
+};
+router.get(
+  "/:section",
+  cors(corsOptions),
+  theGuardianSectionValidator,
+  getSection,
+);
 
 export default router;
